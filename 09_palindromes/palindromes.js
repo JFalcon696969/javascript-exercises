16c0224c6fd5a7500ec3cc6ba02f18119ac56d07
JFalcon696969 <jfalcon696969@gmail.com>
2022-03-06 09:25:50 +0000

    Solved palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let re = /\w+/g
+    let stringArray = string.match(re);
+    string = stringArray.join('');
+    
+    for (i = 0; i < string.length/2; i++){
+        if(string[i] !== string[string.length - 1 - i]){
+            return false
+        }
+    }
+    return true
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
