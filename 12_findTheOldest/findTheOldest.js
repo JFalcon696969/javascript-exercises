9ca03482019bca3c18b202bebdcee1ea4beb9b2a
JFalcon696969 <jfalcon696969@gmail.com>
2022-03-08 19:54:14 +0000

    find the oldest solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((pp, cp) => {
+        ppAge = (pp.yearOfDeath||new Date().getFullYear()) - pp.yearOfBirth;
+        cpAge = (cp.yearOfDeath||new Date().getFullYear()) - cp.yearOfBirth;
 
+        return ppAge > cpAge ? pp : cp; 
+
+    })
 };
 
 // Do not edit below this line
